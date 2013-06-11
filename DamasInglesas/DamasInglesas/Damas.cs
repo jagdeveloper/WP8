77a5b444b5f06e018eac275f42d378888993845f
Jhony Alexander Garcia Gómez <jhonyagarcia@gmail.com>
2013-06-11 09:30:31 -0500

    Aplicacion inicial Damas Inglesas (Taller # 1)

---
+++ DamasInglesas/DamasInglesas/Damas.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace DamasInglesas
+{
+    public class Damas: NotificationEnabledObject
+    {
+
+        public string TURNO = "A";
+        private int fichasA = 12;
+        private int fichasB = 12;
+        public Grid tablero;
+
+        private static Damas instance;
+
+        public static Damas getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Damas();
+
+                
+            }
+            return instance;
+        }
+
+
+        public Damas()
+        {
+            instance = this;
+        }
+
+        public int FichasA
+        {
+            get { return fichasA; }
+            set { fichasA = value;
+            OnPropertyChanged();        
+            }
+        }
+
+        public int FichasB
+        {
+            get { return fichasB; }
+            set { fichasB = value;
+            OnPropertyChanged();
+            }
+        }
+
+        public void ValidarCaptura(Ficha ficha)
+        {
+            Ficha fichaCaptura = PiezaEnPosicion(Int16.Parse(ficha.GetValue(Grid.RowProperty).ToString()),
+                                                Int16.Parse(ficha.GetValue(Grid.ColumnProperty).ToString()),
+                                                ficha.Jugador.Equals(Ficha.JUGADOR_A) ? Ficha.JUGADOR_B : Ficha.JUGADOR_A);
+
+            if (fichaCaptura != null)
+            {
+                tablero.Children.Remove(fichaCaptura);
+                if (fichaCaptura.Jugador.Equals(Ficha.JUGADOR_A))
+                {
+                    FichasA--;
+                }
+                else
+                {
+                    FichasB--;
+                }
+            }
+        }
+
+        public bool PosicionValida(int fila, int columna, Ficha ficha)
+        {
+            bool valida = true;
+            Ficha fichaPosicion = PiezaEnPosicion(fila, columna, ficha.Jugador);
+
+            if (fichaPosicion != null)
+            {
+                valida = false;
+            }
+            
+
+            return valida;
+        }
+
+        private Ficha PiezaEnPosicion(int fila, int columna, string jugador)
+        {
+            Ficha ficha = null;
+
+            if (tablero != null)
+            {
+                for (int i = 0; i < tablero.Children.Count; i++)
+                {
+                    if (tablero.Children[i].GetType() ==  typeof(Ficha))
+                    {
+                        ficha= tablero.Children[i] as Ficha;
+
+                        if (Int16.Parse(ficha.GetValue(Grid.RowProperty).ToString()) == fila &&
+                            Int16.Parse(ficha.GetValue(Grid.ColumnProperty).ToString()) == columna &&
+                            ficha.Jugador.Equals(jugador))
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            ficha = null;
+                        }
+                    }
+                }
+            }
+
+            return ficha;
+        }
+    }
+}
---
+++ DamasInglesas/DamasInglesas/Ficha.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace DamasInglesas
+{
+    public class Ficha : Canvas
+    {
+        public static string JUGADOR_A = "A";
+        public static string JUGADOR_B = "B";
+        
+        private CompositeTransform transform;
+        private double posicionX;
+        private double posicionY;
+
+
+        private string jugador;
+
+        public string Jugador
+        {
+            get { return jugador; }
+            set { jugador = value; }
+        }
+
+        private Grid grilla;
+
+        public Grid Grilla
+        {
+            get { return grilla; }
+            set { grilla = value; }
+        }
+
+        public Ficha()
+        {
+            transform = new CompositeTransform();
+            this.ManipulationStarted += InicioManipulacion;
+            this.ManipulationDelta += Manipulacion;
+            this.ManipulationCompleted += FinManipulacion;
+            this.RenderTransform = transform;
+        }
+
+        
+        private void InicioManipulacion(object sender, System.Windows.Input.ManipulationStartedEventArgs e)
+        {
+            posicionX = 0;
+            posicionY = 0;
+        }
+
+        private void Manipulacion(object sender, System.Windows.Input.ManipulationDeltaEventArgs e)
+        {
+            transform.TranslateX += e.DeltaManipulation.Translation.X;
+            transform.TranslateY += e.DeltaManipulation.Translation.Y;
+        }
+
+        private void FinManipulacion(object sender, System.Windows.Input.ManipulationCompletedEventArgs e)
+        {
+            var ficha = sender as Ficha;
+            int fila;
+            int columna;
+
+            if (Damas.getInstance().TURNO.Equals(Jugador))
+            {
+                if (Jugador.Equals(JUGADOR_B))
+                {
+                    if (posicionY > transform.TranslateY)
+                    {
+                        fila = Int16.Parse(ficha.GetValue(Grid.RowProperty).ToString()) - 1;
+                        if (posicionX > transform.TranslateX)
+                        {
+                            columna = Int16.Parse(ficha.GetValue(Grid.ColumnProperty).ToString()) - 1;
+                        }
+                        else
+                        {
+                            columna = Int16.Parse(ficha.GetValue(Grid.ColumnProperty).ToString()) + 1;
+                        }
+
+                        if (Damas.getInstance().PosicionValida(fila, columna, ficha))
+                        {
+                            ficha.SetValue(Grid.RowProperty, fila);
+                            ficha.SetValue(Grid.ColumnProperty, columna);
+                            Damas.getInstance().TURNO = JUGADOR_A;
+                        }
+                    }
+                }
+                else if (Jugador.Equals(JUGADOR_A))
+                {
+                    if (posicionY < transform.TranslateY)
+                    {
+                        fila = Int16.Parse(ficha.GetValue(Grid.RowProperty).ToString()) + 1;
+                        if (posicionX > transform.TranslateX)
+                        {
+                            columna = Int16.Parse(ficha.GetValue(Grid.ColumnProperty).ToString()) - 1;
+                        }
+                        else
+                        {
+                            columna = Int16.Parse(ficha.GetValue(Grid.ColumnProperty).ToString()) + 1;
+                        }
+                        if (Damas.getInstance().PosicionValida(fila, columna, ficha))
+                        {
+                            ficha.SetValue(Grid.RowProperty, fila);
+                            ficha.SetValue(Grid.ColumnProperty, columna);
+                            Damas.getInstance().TURNO = JUGADOR_B;
+                        }
+                    }
+                }
+            }
+            Damas.getInstance().ValidarCaptura(ficha);
+
+            transform.TranslateX = 0;
+            transform.TranslateY = 0;
+        }
+
+    }
+}
